74ea14b336082600099110392e99c892c2b86050
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-10 19:22:08 +0100

    Ajout de typescript + utilisation d'express

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
+let app = express();
+
+app.get('', function(req:any,res:any) {
+    
+});
+
+
+app.listen("9000");
