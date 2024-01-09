f697e959c761547364d3b00528d4d2788267f6cd
robin <57806687+HiroKX@users.noreply.github.com>
2024-01-09 15:03:30 +0100

    Fin: exo 5

--- ClasseGenerique.ts
+++ ClasseGenerique.ts
@@ -2,42 +2,44 @@
  * TODO
  * Classe permettant d'effectuer des actions sur des tableaux (Surcouche aux arrays)
  */
+export class IterationSurTableau<T> {
 
+    tableau:T[];
+
+    constructor(tableau:T[]) {
+        this.tableau = tableau;
+    }
 
     /**
-     * TODO
      * Fonction ayant pour but d'afficher les données du tableau dans la console
      * @return void
      */
+    printData() {
+        for(let i = 0; i < this.tableau.length; i++) {
+            console.log(this.tableau[i]);
+        }
+    }
 
-
-    /** TODO
+    /**
      * Fonction ayant pour but de trier les données du tableau
      * @return void
      */
+    sortDataByFonction(fonctionDeTri:(p1:T,p2:T) => number) {
+        this.tableau.sort(fonctionDeTri);
+    }
 
-
-    /** TODO
-     * Fonction ayant pour but de trier les données du tableau
-     * @return void
-     */
-
-
-    /** TODO
-     * Fonction ayant pour but de filtrer les données d'un tableau pour ne garder que celle qui <u><b>SONT</b></u> égales à la valeure passée en paramètres
-     * @param valeur valeur servant de comparatif
-     */
-
-    /** TODO
-     * Fonction ayant pour but de filtrer les données d'un tableau pour ne garder que celle qui <u><b>NE SONT PAS</b></u> égales à la valeure passée en paramètres
-     * @param valeur valeur servant de comparatif
-     */
-
-
-    /** TODO
+    /**
      * Fonction ayant pour but d'ajouter une données
      * @param valeur
      * @return T La valeure ajoutée dans le tableau
      */
 
+    addData(valeur:T):T {
+        this.tableau.push(valeur);
+        return valeur;
+    }
 }
+
+
+
+
